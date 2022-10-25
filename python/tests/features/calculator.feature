3386cf1b5a153cb26a0d1eca46c68e5650750112
Tristan Chaput <tristan.chaput@arolla.fr>
2022-10-25 17:09:18 +0200

    change scenario to be like in other languages then use parameterized tests

--- python/tests/features/calculator.feature
+++ python/tests/features/calculator.feature
@@ -6,17 +6,3 @@ Feature: Calculator
 		And the second number is 70
 		When the two numbers are added
 		Then the result should be 120
-
-	Scenario Outline: Add two numbers with templating
-		Given the first number is: <first_number>
-		And the second number is: <second_number>
-		When the two numbers are added
-		Then the result should be: <sum>
-
-		Examples:
-			| first_number | second_number | sum | comment                                               |
-			| 50           | 70            | 120 | Happy path                                            |
-			| 70           | 50            | 120 | Happy path swapped                                    |
-			| 50           | -50           | 0   | A number plus its opposite yields to identity element |
-			| 50           | 0             | 50  | A number plus identity element yields to the number   |
-
--- python/tests/steps/test_calculator.py
+++ python/tests/steps/test_calculator.py
@@ -6,40 +6,21 @@ from currency_converter.calculator import add
 scenarios("../features/calculator.feature")
 
 
-@given("the first number is 50", target_fixture="context")
-@given("the second number is 70")
-def initialize_first_number_equal_to_50_and_the_second_number_is_equal_to_70():
-    return {
-        "first_number": 50,
-        "second_number": 70,
-    }
-
-
-@when("the two numbers are added")
-def add_context_numbers(context) -> None:
-    context["addition_result"] = add(context["first_number"], context["second_number"])
-
-
-@then("the result should be 120")
-def check_if_addition_value_is_120(context) -> None:
-    assert context["addition_result"] == 120
-
-
-@given(parsers.parse("the first number is: {first_number:d}"), target_fixture="context")
+@given(parsers.parse("the first number is {first_number:d}"), target_fixture="context")
 def initialize_first_number(first_number):
     return {"first_number": first_number}
 
 
-@given(parsers.parse("the second number is: {second_number:d}"))
-def initialize_second_number(context, second_number) -> None:
+@given(parsers.parse("the second number is {second_number:d}"))
+def initialize_second_number(context, second_number):
     context["second_number"] = second_number
 
 
 @when("the two numbers are added")
-def add_context_numbers(context) -> None:
+def add_context_numbers(context):
     context["addition_result"] = add(context["first_number"], context["second_number"])
 
 
-@then(parsers.parse("the result should be: {sum:d}"))
-def check_addition(context, sum) -> None:
+@then(parsers.parse("the result should be {sum:d}"))
+def check_addition(context, sum):
     assert context["addition_result"] == sum
