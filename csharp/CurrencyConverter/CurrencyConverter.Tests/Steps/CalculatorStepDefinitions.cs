6277bd218390415f3598fc4b6414f6656188792b
Dimitri Merejkowsky <dimitri@dmerej.info>
2023-02-15 11:55:26 +0100

    cs: Extract Production code in a separate class
    Otherwise it's a bit hard to show what we're testing

--- csharp/CurrencyConverter/CurrencyConverter.Tests/Steps/CalculatorStepDefinitions.cs
+++ csharp/CurrencyConverter/CurrencyConverter.Tests/Steps/CalculatorStepDefinitions.cs
@@ -33,7 +33,10 @@ namespace CurrencyConverter.Tests.Steps
         {
             var first = _scenarioContext.Get<int>("first");
             var second = _scenarioContext.Get<int>("second");
-            _scenarioContext.Add("result", first + second);
+
+            var sum = Calculator.Add(first, second);
+
+            _scenarioContext.Add("result", sum);
         }
 
         [Then("the result should be (.*)")]
